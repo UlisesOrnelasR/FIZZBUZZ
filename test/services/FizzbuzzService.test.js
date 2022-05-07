f9123c0dc886f5b11570efd6c80811b3fb8598c1
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 17:18:36 -0600

    Corrigiendo nombre de la prueba

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -2,7 +2,7 @@ const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
 
 describe('Pruebas para FizzbuzzService', () => {
 
-    test('Test para ver si funciona el ejercicio de FIZZBUZZ',() => {
+    test('Test para que escriba como valor del trick, el score',() => {
         const explorer1 = {name: "Explorer1", score: 1}
         const explorer2 = {name: "Explorer2", score: 3}
         FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
