4de22e0f4442016abc8990ab58ddd338bdc6a6c3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 17:05:20 -0600

    Añadiendo prueba que falle a FizzbuzzService

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,7 +1,14 @@
 const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
 
 describe('Pruebas para FizzbuzzService', () => {
-    test('',() => {
+
+    test('Test para ver si funciona el ejercicio de FIZZBUZZ',() => {
+        const explorer1 = {name: "Explorer1", score: 1}
+        FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+        expect(explorer1).toEqual({name: "Explorer1", score: 1, trick: 1})
 
     })
+
+
 })
