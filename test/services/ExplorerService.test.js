e938ea33e93afeeecfc5b6d2b1b1776fdb455566
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 16:18:41 -0600

    Modificando Reader para que pueda leer otros archivos

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,31 +1,27 @@
-
+const Reader = require('./../../lib/utils/Reader')
 const ExplorerService = require('./../../lib/services/ExplorerService')
 
 describe('Pruebas para la clase ExplorerService',() => {
     
     test('Prueba 1: Filtrando explorers por mission',() => {
-        const explorers = [{mission: 'node'}]
+        const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
         const explorersNode = ExplorerService.filterByMission(explorers, 'node')
         
-        expect(explorersNode.length).toBe(1)
+        expect(explorersNode.length).toBe(10)
     })
 
     test('Prueba 2: Obteniendo cuántos explorers estan en la mission', () => {
-        const explorers = [{mission: 'node'}]
+        const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
         const explorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
 
-        expect(explorersInMission).toBe(1)
+        expect(explorersInMission).toBe(10)
     })
 
     test('Prueba 3: Obteniendo los username de los explorers en la mission ', () => {
-        const explorers = [
-            { githubUsername: "brandonruizmora", mission: "node" },
-            { githubUsername: "carlo", mission: "node" },
-            { githubUsername: "ferch", mission: "java" },
-        ]
+        const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
         const ExplorersMissionNames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-        expect(ExplorersMissionNames[0]).toBe('brandonruizmora')
+        expect(ExplorersMissionNames[0]).toBe('ajolonauta1')
     })
 
 
