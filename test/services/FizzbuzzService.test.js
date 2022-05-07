d3725c7aa9ca1e5b27dda3d86aa969b9fe42cf55
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 17:23:07 -0600

    Agregando la prueba 2, para obtener un valor de FIZZ

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -4,10 +4,18 @@ describe('Pruebas para FizzbuzzService', () => {
 
     test('Test para que escriba como valor del trick, el score',() => {
         const explorer1 = {name: "Explorer1", score: 1}
-        const explorer2 = {name: "Explorer2", score: 3}
         FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
 
         expect(explorer1).toEqual({name: "Explorer1", score: 1, trick: 1})
+    })
+
+    test('Test para cuando la llave de trick sea FIZZ  ', () => {
+        const explorer2 = {name: "Explorer2", score: 3}
+        FizzbuzzService.applyValidationInExplorer(explorer2)
+
+        expect(explorer2).toEqual({name: 'Explorer2', score: 3, trick: 'FIZZ'})
+        
+
     })
 
 
