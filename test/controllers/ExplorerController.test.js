9328307abd153df21d1546ebadccee4bbe533926
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 19:29:25 -0600

    Añadiendo el codigo para pasar la prueba

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -3,7 +3,7 @@ const ExplorerController = require('./../../lib/controllers/ExplorerController')
 describe('Pruebas para ExplorerController', () => {
     test('Prueba filtrando lista de explorers por mission', () => {
         const explorers = ExplorerController.getExplorersByMission("node");
-        expect(explorers.length).toBe(9);   
+        expect(explorers.length).toBe(10);   
 
     })
 }) 
