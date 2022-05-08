23f35498879bdd4c75841dba4c10ea7febadbd1d
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 23:00:20 -0600

    Corrigiendo sintaxis con ESlint

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,24 +1,24 @@
-const ExplorerService = require('../../lib/services/ExplorerService');
-const ExplorerController = require('./../../lib/controllers/ExplorerController')
+const ExplorerService = require("../../lib/services/ExplorerService");
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
-describe('Pruebas para ExplorerController', () => {
+describe("Pruebas para ExplorerController", () => {
 
-    test('Prueba filtrando lista de explorers por mission', () => {
+    test("Prueba filtrando lista de explorers por mission", () => {
         const explorers = ExplorerController.getExplorersByMission("node");
 
         expect(explorers.length).toBe(10);   
-    })
+    });
 
-    test('Probando el metodo getExplorersUsernamesByMission que regresa los usernames de los explorers filtrados por la mision enviada', () => {
-        const usernamesfilters = ExplorerController.getExplorersUsernamesByMission('node')
+    test("Probando el metodo getExplorersUsernamesByMission que regresa los usernames de los explorers filtrados por la mision enviada", () => {
+        const usernamesfilters = ExplorerController.getExplorersUsernamesByMission("node");
 
-        expect(usernamesfilters[0]).toBe('ajolonauta1')
-    })
+        expect(usernamesfilters[0]).toBe("ajolonauta1");
+    });
 
-    test('Prueba 3 probando el metodo getExplorersAmonutByMission que regresa la cantidad de explorers en la mission', () => {
-        const explorers = ExplorerController.getExplorersAmonutByMission('node')
+    test("Prueba 3 probando el metodo getExplorersAmonutByMission que regresa la cantidad de explorers en la mission", () => {
+        const explorers = ExplorerController.getExplorersAmonutByMission("node");
 
-        expect(explorers).toBe(10)
-    })
+        expect(explorers).toBe(10);
+    });
     
-}) 
+}); 
