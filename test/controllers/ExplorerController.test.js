b908e8c511b25c26434eaa66ad4f7b7c14a85eb7
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 19:47:52 -0600

    Agregando prueba 3 que falle

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,15 +1,24 @@
+const ExplorerService = require('../../lib/services/ExplorerService');
 const ExplorerController = require('./../../lib/controllers/ExplorerController')
 
 describe('Pruebas para ExplorerController', () => {
 
     test('Prueba filtrando lista de explorers por mission', () => {
         const explorers = ExplorerController.getExplorersByMission("node");
+
         expect(explorers.length).toBe(10);   
     })
 
     test('Probando el metodo getExplorersUsernamesByMission que regresa los usernames de los explorers filtrados por la mision enviada', () => {
         const usernamesfilters = ExplorerController.getExplorersUsernamesByMission('node')
+
         expect(usernamesfilters[0]).toBe('ajolonauta1')
+    })
+
+    test('Prueba 3 probando el metodo getExplorersAmonutByMission que regresa la cantidad de explorers en la mission', () => {
+        const NumExplorersInMission = ExplorerService.getExplorersAmonutByMission('node')
+
+        expect(NumExplorersInMission).toBe(10)
     })
     
 }) 
