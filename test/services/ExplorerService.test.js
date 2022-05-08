8308771cca5598424c5b355c53693307a1634986
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 18:41:57 -0600

    ESlinter arregló los problemas de estilo

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,28 +1,28 @@
-const Reader = require('./../../lib/utils/Reader')
-const ExplorerService = require('./../../lib/services/ExplorerService')
+const Reader = require("./../../lib/utils/Reader");
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
-describe('Pruebas para la clase ExplorerService',() => {
+describe("Pruebas para la clase ExplorerService",() => {
     
-    test('Prueba 1: Filtrando explorers por mission',() => {
-        const explorers = Reader.readJsonFile('./test/data/ExplorerTestFile.json'); // esto regresa la lista de explorers del archivo
-        const explorersNode = ExplorerService.filterByMission(explorers, 'node')
+    test("Prueba 1: Filtrando explorers por mission",() => {
+        const explorers = Reader.readJsonFile("./test/data/ExplorerTestFile.json"); // esto regresa la lista de explorers del archivo
+        const explorersNode = ExplorerService.filterByMission(explorers, "node");
         
-        expect(explorersNode.length).toBe(10)
-    })
+        expect(explorersNode.length).toBe(10);
+    });
 
-    test('Prueba 2: Obteniendo cuántos explorers estan en la mission', () => {
-        const explorers = Reader.readJsonFile('./test/data/ExplorerTestFile.json'); // esto regresa la lista de explorers del archivo
-        const explorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
+    test("Prueba 2: Obteniendo cuántos explorers estan en la mission", () => {
+        const explorers = Reader.readJsonFile("./test/data/ExplorerTestFile.json"); // esto regresa la lista de explorers del archivo
+        const explorersInMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 
-        expect(explorersInMission).toBe(10)
-    })
+        expect(explorersInMission).toBe(10);
+    });
 
-    test('Prueba 3: Obteniendo los username de los explorers en la mission ', () => {
-        const explorers = Reader.readJsonFile('./test/data/ExplorerTestFile.json'); // esto regresa la lista de explorers del archivo
+    test("Prueba 3: Obteniendo los username de los explorers en la mission ", () => {
+        const explorers = Reader.readJsonFile("./test/data/ExplorerTestFile.json"); // esto regresa la lista de explorers del archivo
         const ExplorersMissionNames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-        expect(ExplorersMissionNames[0]).toBe('ajolonauta1')
-    })
+        expect(ExplorersMissionNames[0]).toBe("ajolonauta1");
+    });
 
 
-})
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,9 +1,9 @@
-const Reader = require('./../../lib/utils/Reader')
+const Reader = require("./../../lib/utils/Reader");
 
-describe('Pruebas para la clase Reader', () => {
-    test('Probando el método para leer un archivo', () => {
+describe("Pruebas para la clase Reader", () => {
+    test("Probando el método para leer un archivo", () => {
         const explorers = Reader.readJsonFile("explorers.json");
-        expect(explorers.length).toBe(15)
-    })
+        expect(explorers.length).toBe(15);
+    });
 
-})
+});
