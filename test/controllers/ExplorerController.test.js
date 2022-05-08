0a8d3def5e2f7056044977dc3551d02a2d14a151
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-05-07 19:40:03 -0600

    Agregando pruba 2 que falle. para el metodo getExplorersUsernamesByMission

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,9 +1,15 @@
 const ExplorerController = require('./../../lib/controllers/ExplorerController')
 
 describe('Pruebas para ExplorerController', () => {
+
     test('Prueba filtrando lista de explorers por mission', () => {
         const explorers = ExplorerController.getExplorersByMission("node");
         expect(explorers.length).toBe(10);   
-
     })
+
+    test('Probando el metodo getExplorersUsernamesByMission que regresa los usernames de los explorers filtrados por la mision enviada', () => {
+        const usernamesfilters = ExplorerController.getExplorersUsernamesByMission('node')
+        expect(usernamesfilters[0]).toBe('ajolonauta1')
+    })
+    
 }) 
